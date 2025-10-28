c2847f9c1b6c2e1343a4e73ecbb33d394e80e39a
Asif <asifjirayat@gmail.com>
2025-10-28 15:42:29 +0530

    Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
